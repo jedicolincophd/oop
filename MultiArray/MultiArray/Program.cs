4aa30727c8817f4b37a4121987e4c9c53fda5e6a
Jedidiah Jan <jedidiahlcolinco@su.edu.ph>
2019-03-31 20:32:35 +0800

    Add files via upload

---
+++ MultiArray/MultiArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TwoDimArray
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[2][] { new int[3] {1, 2, 3}, new int[3] {4, 5, 6} };
+            for (int j = 0; j < array.Length; j += 1) {
+                for (int k = 0; k < array[j].Length; k += 1) {
+                    Console.Write(array[j][k]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
